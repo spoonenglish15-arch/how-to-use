c5a8443a5393ff5488cbf5fc411869f61a2be060
spoonenglish15-arch <spoonenglish15@gmail.com>
2026-09-04 12:03:32 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Firebase 콘솔 > 프로젝트 설정 > 내 앱 > 웹 앱에서 받은 값을 붙여 넣으세요.
-// 따옴표 안의 REPLACE_ME 부분만 실제 값으로 교체하면 됩니다.
 window.firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.firebasestorage.app",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyCBMJQ9eHcjnq1zj9gD-Fox_9z_loYsYy4",
+  authDomain: "how-to-use-app.firebaseapp.com",
+  projectId: "how-to-use-app",
+  storageBucket: "how-to-use-app.firebasestorage.app",
+  messagingSenderId: "186149914027",
+  appId: "1:186149914027:web:c73654372e9acce9533eee",
+  measurementId: "G-ZF9VXZPP2F"
 };
